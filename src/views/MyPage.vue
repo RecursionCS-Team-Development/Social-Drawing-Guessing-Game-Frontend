39e53b5dc151d6d88d6116e6943842521bcdf7a4
haruki111 <yuji515091@gmail.com>
2022-01-12 22:42:47 +0900

    NavigationBar ログイン時、topログイン時の追加 動的変更
    lobby, MyPage, settingsページの追加

---
+++ src/views/MyPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="myPage">
+    <h1>Social Drawing Guessing Game MyPage</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'MyPage',
+  components: {}
+})
+</script>
+
+<style></style>
